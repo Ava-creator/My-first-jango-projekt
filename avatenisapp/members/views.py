b87c292b7c664728e6e649a521abc8f335a9d4f7
Ava <avaobochi@gmail.com>
2025-06-01 10:43:22 +0000

    Starting phase

---
+++ avatenisapp/members/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def members(request):
+    return HttpResponse("Hello world!💝💝💝💝💝 my name is Ava I am 8 yr old Tere maailm!💝💝💝💝💝 minu nimi on Ava ma olen 8 aastat vana.")
